f93e362fc24ef22177350285d3a4d5eed4ee9a51
MatteBlackIT <bouwkamp.IT@gmail.com>
2024-07-25 13:59:07 +0900

    added security and compatability with authentication

--- medihelp-api/src/main/java/com/medihelp/medihelp/controllers/LoginController.java
+++ medihelp-api/src/main/java/com/medihelp/medihelp/controllers/LoginController.java
@@ -1,8 +1,10 @@
+
 package com.medihelp.medihelp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -11,13 +13,24 @@ public class LoginController {
     public String loginGreeting(){
         return "<html>" +
                 "<body>" +
-                "<form>" +
-                "input type='text' name='username'>" +
-                "input type='text' password='password'>" +
+                "<form method='POST' action=/'login'>" +
+                "input type='text' name='username' placeholder='Username'/>" +
+                "input type='password' name='password' placeholder='password'/>" +
                 "input type='submit' value='Login'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+    @PostMapping("/login")
+    @ResponseBody
+    public String handleLogin(@RequestParam String username, @RequestParam String password) {
+        if (authenticate(username,password)) {
+            return "Login successful.";
+        } else {
+            return "Login failed. Try again";
+        }
+    }
+    private boolean authenticate(String username, String password) {
+        return "admin".equals(username) && "password".equals(password);
+    }
 }
-

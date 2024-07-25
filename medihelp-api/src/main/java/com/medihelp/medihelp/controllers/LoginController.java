0dee7daa6b38fa3690a67af0741f4adaf704d007
MatteBlackIT <bouwkamp.IT@gmail.com>
2024-07-25 11:20:43 +0900

    bare bones login controller

--- medihelp-api/src/main/java/com/medihelp/medihelp/controllers/LoginController.java
+++ medihelp-api/src/main/java/com/medihelp/medihelp/controllers/LoginController.java
@@ -1,4 +1,23 @@
 package com.medihelp.medihelp.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 public class LoginController {
+    @GetMapping("login")
+    @ResponseBody
+    public String loginGreeting(){
+        return "<html>" +
+                "<body>" +
+                "<form>" +
+                "input type='text' name='username'>" +
+                "input type='text' password='password'>" +
+                "input type='submit' value='Login'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 }
+

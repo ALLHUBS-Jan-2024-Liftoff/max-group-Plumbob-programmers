0b65122a5d25a5b0d207e95a95e542bba2a1472f
Steven Dombrowski <dombrowskis153@yahoo.com>
2024-08-05 16:39:54 -0400

    Added some styling

--- MediHelp-ui/src/components/ClinicData.jsx
+++ MediHelp-ui/src/components/ClinicData.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
 export default function ClinicData() {
-    const [data, setData] = useState(null); 
+    const [data, setData] = useState([]); 
     const [error, setError] = useState(null);
-
+    // const [allData, setAllData] = useState([]);
+    let allData =[];
+    let allDataPage1 =[];
     useEffect(() => {
         
         let fetchAttempt = 0
         let offset = 0
         const fetchData = async () => {
-            let allData = [];
-            while (fetchAttempt < 5){
+            
+            while (fetchAttempt < 1){
             try {
-                const response = await fetch(`https://cors-anywhere.herokuapp.com/https://data.cms.gov/provider-data/api/1/datastore/query/xubh-q36u/0?offset=${offset}&limit=500`, {
+                const response = await fetch(`https://cors-anywhere.herokuapp.com/https://data.cms.gov/provider-data/api/1/datastore/query/xubh-q36u/0?offset=${offset}&limit=100`, {
                     headers: {
                         'X-Requested-With': 'XMLHttpRequest',
                     }
@@ -23,18 +25,30 @@ export default function ClinicData() {
                 }
 
                 const data = await response.json(); 
+                setData(data.results)
                 
-                setData(data);
+                // console.log(data.results)
+                if(data.results.state = "AL"){
+                    allData.push(data.results)
+                }
+                
+                
+               
+                
             } catch (error) {
                 setError(error); 
                 console.error('Error fetching data:', error);
             }
-          
+            
             fetchAttempt++;
             offset += 500;
+            // console.log(allData[1])
+            allDataPage1 = allData[1]
+            console.log(allDataPage1)
         };}
-
         fetchData();
+     
+
     }, []); 
 
     if (error) {
@@ -42,19 +56,27 @@ export default function ClinicData() {
     }
 
     return (
-        <div>
-            {data ? (
-                <div>
-                  
-                    {data.results.map((item, index) => (
-                        <div key={index}>
-                            <h3>{item.facility_name}</h3>
+        <div >
+        {data ? (
+            <div >
+                {data.map((item, index) => (
+                    <div key={index} className='inline-flex w-2/4 '>
+                        <div className='grid grid-rows-3 grid-flow-col gap-4 px-4 py-4 leading-10 border-solid border-2 bg-gray-700 bg-opacity-5'>
+                        <h3 className='row-span-3 bg-gray-500 bg-opacity-5 '>{item.facility_name}</h3>
+                        <h3 className='col-span-2 bg-gray-500 bg-opacity-5' >{item.address}</h3>
+                        <div className='row-span-2 col-span-2 bg-gray-500 bg-opacity-5 '>
+                        <h3 >{item.citytown}</h3>
+                        <h3 >{item.state}</h3>
                         </div>
-                    ))}
-                </div>
-            ) : (
-                <div>Loading...</div>
-            )}
-        </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        ) : (
+            <div>Loading...</div>
+        )}
+    </div>
+  
+        
     );
 }

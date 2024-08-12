934e9c687795bd9f1189466b6ce59548ecdda2b3
MatteBlackIT <bouwkamp.IT@gmail.com>
2024-08-12 19:35:53 +0900

    adjusted the controller. frontend connection in progress.

--- medihelp-api/src/main/java/com/medihelp/medihelp/controllers/LoginController.java
+++ medihelp-api/src/main/java/com/medihelp/medihelp/controllers/LoginController.java
@@ -1,34 +1,36 @@
 
 package com.medihelp.medihelp.controllers;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @Controller
 public class LoginController {
     @GetMapping("login")
     @ResponseBody
     public String loginGreeting(){
-        return "<html>" +
-                "<body>" +
-                "<form method='POST' action=/'login'>" +
-                "input type='text' name='username' placeholder='Username'/>" +  //placeholder form
-                "input type='password' name='password' placeholder='password'/>" +
-                "input type='submit' value='Login'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "Welcome back! Please enter your details.";
     }
     @PostMapping("/login") //authentication bones
     @ResponseBody
-    public String handleLogin(@RequestParam String username, @RequestParam String password) {
-        if (authenticate(username,password)) {
+    public String handleLogin(@RequestBody LoginRequest loginRequest) {
+        if (authenticate(loginRequest.getUsername(), loginRequest.getPassword())) {
             return "Login successful.";
         } else {
             return "Login failed. Try again";
         }
+    }
+
+    public static class LoginRequest {
+        private String username;
+        private String password;
+
+        // Getters and setters
+        public String getUsername() { return username; }
+        public void setUsername(String username) { this.username = username; }
+
+        public String getPassword() { return password; }
+        public void setPassword(String password) { this.password = password; }
     }
     private boolean authenticate(String username, String password) {
         //put in authentication logic

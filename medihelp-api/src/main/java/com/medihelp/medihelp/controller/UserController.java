0edc5a1718a62b5006108946315cb410c3131a1c
Steven Dombrowski <dombrowskis153@yahoo.com>
2024-08-15 16:42:23 -0400

    Synced backend progress with frontend progress on one branch

---
+++ medihelp-api/src/main/java/com/medihelp/medihelp/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.medihelp.medihelp.controller;
+
+
+import com.medihelp.medihelp.model.User;
+import com.medihelp.medihelp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    public Iterable<User> displayUsers() {
+        return userRepository.findAll();
+}
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@RequestParam Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
